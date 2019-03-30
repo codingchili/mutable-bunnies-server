d6ecd7545f31ae9fe940537f9f929d497afbbdef
Robin Duda <robin.duda@idainfront.se>
2019-03-30 14:24:05 +0100

    always re-add skybox in initialization because root and stage is cleared

--- prototype/website/resources/script/skybox.js
+++ prototype/website/resources/script/skybox.js
@@ -5,24 +5,16 @@ window.Skybox = class {
 
     constructor() {
         this.clouds = [];
-        this.background = null;
     }
 
     init(skybox) {
-        if (!this.background) {
-            this._create(skybox);
-        } else {
-            this._setTint(skybox);
-        }
-    }
-
-    _create(skybox) {
         assetLoader.load(background => {
             this.background = background;
 
             let ratio = Math.max(window.innerWidth / 2048, window.innerHeight / 1536);
             background.scale.x = ratio;
             background.scale.y = ratio;
+            background.tint = parseInt(skybox.sky.replace('#', '0x'));
 
             game.root.addChildAt(background, 0);
             for (let cloud = 1; cloud <= 3; cloud++) {
@@ -31,24 +23,14 @@ window.Skybox = class {
                         let cloud = new PIXI.Sprite(loaded.texture);
                         cloud.y = Math.random() * window.innerHeight;
                         cloud.x = -cloud.width;
+                        cloud.tint = parseInt(skybox.clouds.replace('#', '0x'));
                         cloud.velocity = Math.random() + 0.05;
                         this.clouds.push(cloud);
                         game.root.addChildAt(cloud, 1);
                     }
                 }, `game/map/clouds/${cloud}.png`);
             }
-
-            this._setTint(skybox);
-
         }, 'game/map/clouds/skybox_grey.png');
-    }
-
-    _setTint(skybox) {
-        this.background.tint = parseInt(skybox.sky.replace('#', '0x'));
-
-        for (let cloud of this.clouds) {
-            cloud.tint = parseInt(skybox.clouds.replace('#', '0x'));
-        }
     }
 
     update() {
