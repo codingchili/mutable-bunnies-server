9417d3c89abaeeb013ae6b545f76a60c65c28d02
Robin Duda <robin.duda@idainfront.se>
2019-08-08 22:49:37 +0200

    offline support network-then-cache service worker + pwa install status.

--- prototype/website/service_worker.js
+++ prototype/website/service_worker.js
@@ -1,54 +1,72 @@
 /*
  * Service worker.
+ *
+ * - prefer network.
+ * - fallback on fail.
  */
+let CACHE_NAME = 'bunny-cache-v22';
+let urlsToCache = ['/', './index.html'];
 
-let CACHE_NAME = 'bunny-cache-v5';
-let urlsToCache = ['/index.html'];
+let failures = 0;
+let offline = false;
 
-self.addEventListener('install', function(event) {
+console.log('using service worker cache ' + CACHE_NAME);
+
+self.addEventListener('install', (event) => {
+    // remove all old caches, because reasons.
+    for (let i = 0; i < 1000; i++) {
+        caches.delete(`bunny-cache-v${i}`);
+    }
+
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(function(cache) {
+            .then((cache) => {
                 return cache.addAll(urlsToCache);
             })
     );
 });
 
+self.addEventListener('activate', () => {
+    console.log('service worker activated on ' + CACHE_NAME);
+});
 
-self.addEventListener('fetch', function(event) {
-    return false;
-    /*if (event.request.url.match("/resources/!*")) {
-        return false;
-    }*/
+self.addEventListener('message', (event) => {
+    console.log("service worker query: " + event.data);
+    event.ports[0].postMessage({offline: offline});
+});
 
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                // Cache hit - return response
-                if (response) {
-                    return response;
-                }
-
-                return fetch(event.request).then((response) => {
-                        // Check if we received a valid response
-                        if(!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
-
-                        // IMPORTANT: Clone the response. A response is a stream
-                        // and because we want the browser to consume the response
-                        // as well as the cache consuming the response, we need
-                        // to clone it so we have two streams.
-                        let responseToCache = response.clone();
-
-                        caches.open(CACHE_NAME)
-                            .then(function(cache) {
-                                cache.put(event.request, responseToCache);
-                            });
-
-                        return response;
-                    }
-                );
+self.addEventListener('fetch', (event) => {
+    if (offline) {
+        event.respondWith(caches.open(CACHE_NAME)
+            .then(cache => {
+                return caches.match(event.request)
             })
-    );
+        );
+    } else {
+        event.respondWith(
+            caches.open(CACHE_NAME)
+                .then(cache => {
+                    return fetch(event.request)
+                        .then(response => {
+                            let clone = response.clone();
+
+                            cache.match(event.request)
+                                .then(() => {
+                                    cache.put(event.request, clone);
+                                });
+                            return response;
+                        })
+                        .catch(() => {
+                            failures++;
+                            if (!offline && failures > 8) {
+                                console.log('offline detected: defaulting to cache.');
+                                offline = true;
+                            }
+                            return caches.match(event.request);
+                        })
+
+                })
+        );
+    }
 });
