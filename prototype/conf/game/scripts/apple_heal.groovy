bf0c00b8013bb96c6bfaaa8df3fa26d86b9a6274
Robin Duda <robin.duda@idainfront.se>
2019-08-10 17:23:10 +0200

    reduced the number of energy apples yield + increased health returned.

--- prototype/conf/game/scripts/apple_heal.groovy
+++ prototype/conf/game/scripts/apple_heal.groovy
@@ -1,6 +1,6 @@
 // heal the user by 24 hp.
 // access to the item object!
 
-spells.heal(source, (item.rarity.ordinal() + 1) * 16)
-spells.energy(source, (item.rarity.ordinal() + 1) * 48)
+spells.heal(source, (item.rarity.ordinal() + 1) * 24)
+spells.energy(source, (item.rarity.ordinal() + 1) * 16)
 source.getSpells().setGcd(500)
