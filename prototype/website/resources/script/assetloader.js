badb5c672aab6316ef65cc8583c446f20cf61991
Robin Duda <robin.duda@idainfront.se>
2019-08-19 01:14:19 +0200

    implement click-through for transparent sprites interactivity.

--- prototype/website/resources/script/assetloader.js
+++ prototype/website/resources/script/assetloader.js
@@ -7,6 +7,9 @@ window.AssetLoader = class AssetLoader {
         this.loading = false;
         this.queue = [];
         this.resources = PIXI.Loader.shared.resources;
+
+        // install custom features to pixi.
+        this.install();
     }
 
     load(callback, assetName) {
@@ -71,6 +74,18 @@ window.AssetLoader = class AssetLoader {
                     resource = PIXI.Loader.shared.resources[current.assetName].data;
                 } else {
                     resource = new PIXI.Sprite(PIXI.Loader.shared.resources[current.assetName].texture);
+                    resource.hitArea = new PIXI.Rectangle(0, 0, resource._texture.width, resource._texture.height);
+                    resource.hitArea.contains = (x, y) => {
+                        let area = resource.hitArea;
+
+                        if (x >= area.x && x < area.x + area.width) {
+                            if (y >= area.y && y < area.y + area.height) {
+                                // make sure alpha is not 0, click through transparency.
+                                return resource.getColorAt(x, y)[3] > 0;
+                            }
+                        }
+                        return false;
+                    };
                 }
                 current.callback(resource);
             }
@@ -87,6 +102,88 @@ window.AssetLoader = class AssetLoader {
         this.completers.forEach((completer) => {
             completer();
         });
+    }
+
+    /**
+     * https://github.com/pixijs/pixi.js/wiki/v5-Hacks#pixel-perfect-interaction
+     */
+    install() {
+        const genColorMap = this.genColorMap;
+        PIXI.Sprite.prototype.getColorAt = function (eX, eY) {
+            const width = this._texture.orig.width;
+            const height = this._texture.orig.height;
+            const x1 = -width * this.anchor.x;
+            let y1 = 0;
+
+            let flag = false;
+
+            if (eX >= x1 && eX < x1 + width) {
+                y1 = -height * this.anchor.y;
+
+                if (eY >= y1 && eY < y1 + height) {
+                    flag = true;
+                }
+            }
+
+            if (!flag) {
+                return [0, 0, 0, 0];
+            }
+
+            // bitmap check
+            const tex = this.texture;
+            const baseTex = this.texture.baseTexture;
+            if (!baseTex.colormap) {
+                if (!genColorMap(baseTex)) {
+                    return [0, 0, 0,];
+                }
+            }
+
+            const colormap = baseTex.colormap;
+            const data = colormap.data;
+            const res = baseTex.resolution;
+
+            // this does not account for rotation yet!!!
+            let dx = Math.round((eX - x1 + tex.frame.x) * res);
+            let dy = Math.round((eY - y1 + tex.frame.y) * res);
+            let num = dx + dy * colormap.width;
+            return [data[num * 4], data[num * 4 + 1], data[num * 4 + 2], data[num * 4 + 3]];
+        }
+    }
+
+    /**
+     * https://github.com/pixijs/pixi.js/wiki/v5-Hacks#pixel-perfect-interaction
+     *
+     * @param baseTex
+     * @returns {boolean}
+     */
+    genColorMap(baseTex) {
+        if (!baseTex.resource) {
+            //renderTexture
+            return false;
+        }
+        const imgSource = baseTex.resource.source;
+        let canvas = null;
+        if (!imgSource) {
+            return false;
+        }
+        let context = null;
+        if (imgSource.getContext) {
+            canvas = imgSource;
+            context = canvas.getContext('2d');
+        } else if (imgSource instanceof Image) {
+            canvas = document.createElement('canvas');
+            canvas.width = imgSource.width;
+            canvas.height = imgSource.height;
+            context = canvas.getContext('2d');
+            context.drawImage(imgSource, 0, 0);
+        } else {
+            //unknown source;
+            return false;
+        }
+
+        const w = canvas.width, h = canvas.height;
+        baseTex.colormap = context.getImageData(0, 0, w, h);
+        return true;
     }
 };
 
