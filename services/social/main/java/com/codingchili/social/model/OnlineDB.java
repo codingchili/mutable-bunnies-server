9dbf6d88bb40b8a43ecc4f32eaeffdc3ef8179ec
Robin Duda <robin.duda@idainfront.se>
2019-08-27 00:37:55 +0200

    rename friendlist database + javadoc onlinedb.

--- services/social/main/java/com/codingchili/social/model/OnlineDB.java
+++ services/social/main/java/com/codingchili/social/model/OnlineDB.java
@@ -1,14 +1,12 @@
 package com.codingchili.social.model;
 
-import com.codingchili.social.configuration.SocialContext;
-
 import java.util.*;
 
 import com.codingchili.core.context.CoreContext;
 
 /**
  * @author Robin Duda
- *
+ * <p>
  * Tracks accounts that are online for friendlists and chat.
  */
 public class OnlineDB {
@@ -16,17 +14,18 @@ public class OnlineDB {
     private Map<String, Set<String>> connected = new HashMap<>();
 
     /**
-     *
-     * @param context
+     * @param context creates the database for the given context.
      */
     public OnlineDB(CoreContext context) {
         this.context = context;
     }
 
     /**
+     * Adds a new account to the online database. If the account already is online
+     * then the realm will be added to their online presence.
      *
-     * @param account
-     * @param realm
+     * @param account the account to indicate as online.
+     * @param realm   the realm for which the account is online.
      */
     public void add(String account, String realm) {
         connected.computeIfAbsent(account, key -> new HashSet<>());
@@ -34,9 +33,10 @@ public class OnlineDB {
     }
 
     /**
+     * Removes an accounts online presence from the given realm.
      *
-     * @param account
-     * @param realm
+     * @param account the account to be removed.
+     * @param realm   the realm that the account is to be removed from.
      */
     public void remove(String account, String realm) {
         Set<String> realms = connected.getOrDefault(account, new HashSet<>());
@@ -50,18 +50,18 @@ public class OnlineDB {
     }
 
     /**
-     *
-     * @param account
-     * @return
+     * @param account the account to check if online or not.
+     * @return true if the account is online on at least one realm.
      */
     public boolean is(String account) {
         return connected.containsKey(account);
     }
 
     /**
+     * Retrieves a set of all of the realms that the given account is considered online for.
      *
-     * @param account
-     * @return
+     * @param account the account to retrieve the online realms for.
+     * @return a set of realm names.
      */
     public Set<String> realms(String account) {
         return connected.getOrDefault(account, new HashSet<>());
