99c939888ad80055804f6b73922c5d3fd7c7cff4
Robin Duda <robin.duda@idainfront.se>
2020-12-03 14:19:09 +0100

    fixed a test fail caused by empty testclass

--- services/realm/test/java/com/codingchili/instance/SpellEngineTest.java
+++ services/realm/test/java/com/codingchili/instance/SpellEngineTest.java
@@ -1,11 +1,15 @@
 package com.codingchili.instance;
 
 import io.vertx.ext.unit.junit.VertxUnitRunner;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(VertxUnitRunner.class)
 public class SpellEngineTest {
 
-
+    @Test
+    public void test() {
+        // build fails without at least one test.
+    }
 
 }
