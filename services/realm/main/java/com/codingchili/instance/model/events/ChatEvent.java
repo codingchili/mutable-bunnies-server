98aa112dde2210cb99920709c39b5e8d65d40100
Robin Duda <rduda@kth.se>
2021-01-16 19:30:18 +0100

    improvements to player chat and text formatting.

--- services/realm/main/java/com/codingchili/instance/model/events/ChatEvent.java
+++ services/realm/main/java/com/codingchili/instance/model/events/ChatEvent.java
@@ -11,9 +11,22 @@ public class ChatEvent implements Event {
     private String source;
     private String text;
 
+    private ChatEvent() {}
+
     public ChatEvent(Entity source, String text) {
         this.source = source.getId();
         this.text = text;
+    }
+
+    public static ChatEvent system(String text) {
+        return new ChatEvent()
+                .setSystem(true)
+                .setText(text);
+    }
+
+    public ChatEvent setText(String text) {
+        this.text = text;
+        return this;
     }
 
     public String getText() {
