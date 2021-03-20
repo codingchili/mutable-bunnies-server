74b98b2ca2c9fab7a389a8d7659f6004628599e4
Robin Duda <robin.duda@idainfront.se>
2021-03-20 21:01:18 +0100

    region picker + enable banking microservice

--- services/banking/main/java/com/codingchili/banking/model/AuctionDB.java
+++ services/banking/main/java/com/codingchili/banking/model/AuctionDB.java
@@ -2,19 +2,10 @@ package com.codingchili.banking.model;
 
 import com.codingchili.core.storage.AsyncStorage;
 
-// text search
-// seller search
-// navleaf searches ..
-   // favorites, active, sold, not sold, won, leading, overbid, lost
-
 public class AuctionDB implements AsyncAuctionStore {
     private AsyncStorage<Auction> auctions;
 
     public AuctionDB(AsyncStorage<Auction> auctions) {
         this.auctions = auctions;
-    }
-
-    public void foo() {
-
     }
 }
