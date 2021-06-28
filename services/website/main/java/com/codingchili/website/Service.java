ddd27b28985d968330784b9ee36935b29ddbbdfc
Robin Duda <robin.duda@idainfront.se>
2021-06-28 13:51:51 +0200

    enhanced region picker + error handling/logging

--- services/website/main/java/com/codingchili/website/Service.java
+++ services/website/main/java/com/codingchili/website/Service.java
@@ -2,19 +2,17 @@ package com.codingchili.website;
 
 import com.codingchili.website.configuration.WebserverContext;
 import com.codingchili.website.configuration.WebserverSettings;
-import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.http.HttpServerOptions;
-import io.vertx.ext.web.Http2PushMapping;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.ext.web.handler.StaticHandler;
 
 import java.nio.file.Path;
-import java.util.List;
 
 import com.codingchili.core.context.CoreContext;
 import com.codingchili.core.listener.CoreService;
+import com.codingchili.core.logging.Logger;
 
 import static com.codingchili.core.context.FutureHelper.untyped;
 
@@ -24,11 +22,14 @@ import static com.codingchili.core.context.FutureHelper.untyped;
  * Service for the webserver.
  */
 public class Service implements CoreService {
+    public static final long KB_256 = 256_000;
     private WebserverContext core;
     private WebserverSettings settings;
+    private Logger logger;
 
     @Override
     public void init(CoreContext core) {
+        this.logger = core.logger(getClass());
         this.core = new WebserverContext(core);
         this.settings = this.core.service();
     }
@@ -37,7 +38,11 @@ public class Service implements CoreService {
     public void start(Promise<Void> start) {
         core.blocking((blocking) -> {
             Router router = Router.router(core.vertx());
-            router.route().handler(BodyHandler.create());
+            router.route().handler(
+                    BodyHandler.create()
+                            .setBodyLimit(KB_256)
+                            .setDeleteUploadedFilesOnEnd(true)
+            );
 
             router.route("/*").handler(ctx -> {
                 if (ctx.request().path().equals("/")) {
@@ -51,7 +56,7 @@ public class Service implements CoreService {
                     .setCacheEntryTimeout(300_000L)
                     .setSendVaryHeader(false)
                     .setIndexPage(settings.getStartPage())
-                    .setHttp2PushMapping(List.of(new Http2PushMapping()))
+                    //.setHttp2PushMapping(List.of(new Http2PushMapping()))
                     .setWebRoot(settings.getResources()));
 
             router.route().last().handler(ctx -> {
@@ -68,6 +73,7 @@ public class Service implements CoreService {
 
             core.vertx().createHttpServer(options)
                     .requestHandler(router)
+                    .exceptionHandler(logger::onError)
                     .listen(settings.getListener().getPort(), untyped(blocking));
         }, start);
     }
